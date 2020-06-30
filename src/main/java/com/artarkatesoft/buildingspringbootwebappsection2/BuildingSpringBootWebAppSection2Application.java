fbf0dab0386275043543ba224d1506f6d30a6888
admin <d.art.shishkin@gmail.com>
2020-06-30 23:40:48 +0300

    init

---
+++ src/main/java/com/artarkatesoft/buildingspringbootwebappsection2/BuildingSpringBootWebAppSection2Application.java
@@ -0,0 +1,13 @@
+package com.artarkatesoft.buildingspringbootwebappsection2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingSpringBootWebAppSection2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildingSpringBootWebAppSection2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/artarkatesoft/buildingspringbootwebappsection2/BuildingSpringBootWebAppSection2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artarkatesoft.buildingspringbootwebappsection2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingSpringBootWebAppSection2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
